8889f834d60b712d0367ab04008c636dccd6b031
githhpradz-cmd <githhpradz@gmail.com>
2026-08-12 07:47:15 +0700

    Add files via upload

---
+++ dptabab3.py
@@ -0,0 +1,19 @@
+Nama = "Pradipta"
+Umur = "16"
+Tinggi = "170"
+Lulus = "True"
+
+print("Nama : ", Nama)
+print("Umur : ",Umur)
+print("Tinggi : ",Tinggi)
+print("Lulus : ", Lulus)
+
+Produk = "Sepatu Lari"
+Stock = "15"
+Harga = "250"
+Tersedia = "True"
+
+print("Nama_Produk : ",Produk)
+print("Stock_Tersedia : ",Stock)
+print("Harga : ", Harga)
+print("Tersedia_di_Toko = ",Tersedia)
